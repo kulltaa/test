e4744f89338612562b13e99ec0743c347fb7da66
kulltaa <2359media@2359medias-MacBook-Air.local>
2016-11-24 18:03:13 +0700

    Refactor

---
+++ test/unit/server/plugins/auth.spec.js
@@ -0,0 +1,67 @@
+/* eslint-disable */
+const Hapi = require('hapi');
+const Promise = require('bluebird');
+const sinon = require('sinon');
+const chai = require('chai');
+const authPlugin = require('../../../../libs/plugins/auth');
+const authToken = require('../../../../libs/plugins/auth/token');
+
+const expect = chai.expect;
+
+describe('Auth', () => {
+  let stub;
+  let server;
+
+  beforeEach((done) => {
+    stub = sinon.stub(authToken.options, 'validateFunc');
+
+    server = new Hapi.Server();
+    server.connection();
+
+    server.register(authPlugin, done);
+  })
+
+  afterEach((done) => {
+    stub.restore();
+    server.stop(done);
+  })
+
+  it.only('should return credentials when request is valid', (done) => {
+    const credentials = {
+      username: 'some-username'
+    };
+
+    stub.yields(null, true, credentials);
+
+    const token = 'some-access-token';
+    const options = {
+      method: 'GET',
+      url: '/',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, reply) {
+        request.server.auth.test('auth-access-token', request, (error, credentials) => {
+          if (error) {
+            return reply(error);
+          }
+
+          return reply(credentials);
+        });
+      }
+    });
+
+    server.inject(options, (res) => {
+      sinon.assert.calledWith(stub, token);
+      expect(res.result).to.include.keys('username');
+      expect(res.result.username).to.equal(credentials.username);
+
+      done();
+    });
+  });
+});
--- test/unit/server/plugins/response.spec.js
+++ test/unit/server/plugins/response.spec.js
@@ -1,19 +1,18 @@
 /* eslint-disable */
 const Hapi = require('hapi');
-const sinon = require('sinon');
-const Promise = require('bluebird');
 const chai = require('chai');
 const responsePlugin = require('../../../../libs/plugins/response');
-require('sinon-as-promised')(Promise);
 
 const expect = chai.expect;
 
 describe('Response', () => {
   let server;
 
-  beforeEach(() => {
+  beforeEach((done) => {
     server = new Hapi.Server();
     server.connection();
+
+    server.register(responsePlugin, done);
   })
 
   afterEach((done) => {
@@ -23,125 +22,95 @@ describe('Response', () => {
   })
 
   it('should register response plugin success', (done) => {
-    server.register(responsePlugin, (error) => {
-      if (error) {
-        return done(error);
-      }
+    server.ext('onRequest', (request, reply) => {
+      expect(reply.success).to.be.an.instanceof(Function);
+      expect(reply.notFound).to.be.an.instanceof(Function);
+      expect(reply.serverError).to.be.an.instanceof(Function);
 
-      server.ext('onRequest', (request, reply) => {
-        expect(reply.success).to.be.an.instanceof(Function);
-        expect(reply.notFound).to.be.an.instanceof(Function);
-        expect(reply.serverError).to.be.an.instanceof(Function);
-
-        done();
-      });
-
-      server.inject('/');
+      done();
     });
+
+    server.inject('/');
   });
 
   it('reply success should return response and status code 200', (done) => {
-    server.register(responsePlugin, (error) => {
-      if (error) {
-        return done(error);
+    const expectedResponse = {
+      data: []
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler(request, reply) {
+        return reply.success(expectedResponse);
       }
+    });
 
-      const expectedResponse = {
-        data: []
-      };
+    server.inject('/', (res) => {
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.deep.equal(expectedResponse);
 
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler(request, reply) {
-          return reply.success(expectedResponse);
-        }
-      });
-
-      server.inject('/', (res) => {
-        expect(res.statusCode).to.equal(200);
-        expect(res.result).to.deep.equal(expectedResponse);
-
-        done();
-      });
+      done();
     });
   });
 
   it('reply notFound should return status code 404', (done) => {
-    server.register(responsePlugin, (error) => {
-      if (error) {
-        return done(error);
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler(request, reply) {
+        return reply.notFound();
       }
+    });
 
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler(request, reply) {
-          return reply.notFound();
-        }
-      });
+    server.inject('/', (res) => {
+      expect(res.statusCode).to.equal(404);
 
-      server.inject('/', (res) => {
-        expect(res.statusCode).to.equal(404);
-
-        done();
-      });
+      done();
     });
   });
 
   it('in development, reply serverError should return response and status code 500', (done) => {
-    server.register(responsePlugin, (error) => {
-      if (error) {
-        return done(error);
+    const expectedResponse = {
+      data: []
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler(request, reply) {
+        return reply.serverError(expectedResponse);
       }
+    });
 
-      const expectedResponse = {
-        data: []
-      };
+    server.inject('/', (res) => {
+      expect(res.statusCode).to.equal(500);
+      expect(res.result).to.deep.equal(expectedResponse);
 
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler(request, reply) {
-          return reply.serverError(expectedResponse);
-        }
-      });
-
-      server.inject('/', (res) => {
-        expect(res.statusCode).to.equal(500);
-        expect(res.result).to.deep.equal(expectedResponse);
-
-        done();
-      });
+      done();
     });
   });
 
   it('in production, reply serverError should return empty response and status code 500', (done) => {
     process.env.NODE_ENV = 'production';
 
-    server.register(responsePlugin, (error) => {
-      if (error) {
-        return done(error);
+    const expectedResponse = {
+      data: []
+    };
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler(request, reply) {
+        return reply.serverError(expectedResponse);
       }
+    });
 
-      const expectedResponse = {
-        data: []
-      };
+    server.inject('/', (res) => {
+      expect(res.statusCode).to.equal(500);
+      expect(res.result).to.not.deep.equal(expectedResponse);
 
-      server.route({
-        method: 'GET',
-        path: '/',
-        handler(request, reply) {
-          return reply.serverError(expectedResponse);
-        }
-      });
-
-      server.inject('/', (res) => {
-        expect(res.statusCode).to.equal(500);
-        expect(res.result).to.not.deep.equal(expectedResponse);
-
-        done();
-      });
+      done();
     });
   });
 });
